eea5bd89b66d32661f0ca89a2cc9e6ab9af2d8ce
amunishkin <munishkinalex@gmail.com>
2020-03-28 10:57:17 -0700

    initial update

---
+++ _admin/mailer.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# send - sends files to email ($i)
+#
+
+EXITCODE=0
+
+ASSIGNMENT = Lab1
+SUBJECT    = "CSE-20-01 ${ASSIGNMENT} Grade"
+DIR        = /afs/cats.ucsc.edu/class/cmps011-pt.s15/pa6
+
+FILE=grade
+#FILE=corrections
+
+#
+# iterates through directory list and checks if directory exist
+#
+for i in $(ls $DIR)
+do
+	if [ -d "${DIR}/$i" ]
+	then
+#
+# sends files
+#
+		if [ -e "${DIR}/${i}/$FILE" ]
+		then
+			echo "sending mail to ${i}@ucsc.edu"
+			cat ${DIR}/${i}/$FILE | mailx -s "$SUBJECT" ${i}@ucsc.edu >& tmp
+			sleep 3
+			if [ -e tmp -a $(wc -l < tmp) -eq 0 ]
+			then
+				echo "mail to ${i} is sent"
+			else
+				echo "mail to ${i} is not sent"
+				cat tmp
+			fi
+		else 
+			echo "could not sent email to ${i}@ucsc.edu"
+			echo "$FILE does not exist"
+		fi	
+     	else
+#
+# prints error message if directory does not exist
+#
+		echo "$i: directory does not exist" >&2
+		echo
+		EXITCODE=2
+	fi
+done
+echo "all mails are sent" | tee note
+
+#
+# send notice to my email
+#
+cat note | mailx -s "$SUBJECT [mailx DONE]" amunishk@ucsc.edu
+exit $EXITCODE
