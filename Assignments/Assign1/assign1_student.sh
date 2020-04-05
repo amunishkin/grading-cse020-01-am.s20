92d2602dd1ce0c6bec1b13a60730b2ffb045cbdf
amunishkin <munishkinalex@gmail.com>
2020-04-05 13:01:57 -0700

    ls instead of ls . | grep ...

--- Assignments/Assign1/assign1_student.sh
+++ Assignments/Assign1/assign1_student.sh
@@ -39,7 +39,7 @@ echo "" >> $REPORT
 # checks that program runs ----------------------
 #
 # find *.py filenames : extra check
-ls . | grep "[.]py" > py.files 
+ls ./*.py > py.files 
 cat py.files
 if [ -s py.files ]; then        # Only if py file exists
   while read line; do
